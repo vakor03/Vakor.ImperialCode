77a2165095eb93e4603256116be730372d249df6
Vakor <vakor.dev@gmail.com>
2022-02-19 00:00:49 +0200

    18.02 Lab1

---
+++ Vakor.Paradigms.Lab1/task1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.IO;
+
+
+string[] stopWords = {"the", "in", "a", "an", "for", "to", "on"};
+
+string pathToFile = "../../../input.txt";
+string outputPath = "../../../output.txt";
+
+string[] words = Array.Empty<string>();
+int[] repeatedWordsCount = Array.Empty<int>();
+
+int wordsCount = 0;
+
+int maxWordsCount = 25;
+string word = "";
+
+StreamReader streamReader = new(pathToFile);
+
+readFile:
+{
+    if (streamReader.EndOfStream)
+    {
+        goto finishReading;
+    }
+
+    char symbol = (char) streamReader.Read();
+
+    if (symbol is >= 'A' and <= 'Z')
+    {
+        word += (char) (symbol + 32);
+
+        if (!streamReader.EndOfStream)
+        {
+            goto readFile;
+        }
+    }
+    else if (symbol is >= 'a' and <= 'z' || word != "" && symbol is '-' or '\'')
+    {
+        word += symbol;
+
+        if (!streamReader.EndOfStream)
+        {
+            goto readFile;
+        }
+    }
+
+    if (word != "")
+    {
+        int i = 0;
+
+        checkForStopWord:
+        {
+            if (i == stopWords.Length)
+            {
+                i = 0;
+                goto checkWordInArray;
+            }
+
+            if (stopWords[i] == word)
+            {
+                word = String.Empty;
+                goto readFile;
+            }
+
+            i++;
+            goto checkForStopWord;
+        }
+
+
+        checkWordInArray:
+        {
+            if (i == wordsCount)
+            {
+                goto addNewWord;
+            }
+
+            if (words[i] == word)
+            {
+                repeatedWordsCount[i]++;
+                word = String.Empty;
+                goto readFile;
+            }
+
+            i++;
+            goto checkWordInArray;
+        }
+
+        addNewWord:
+        {
+            if (wordsCount != words.Length)
+            {
+                goto addWordToArray;
+            }
+        }
+
+
+        // expanding array
+        {
+            string[] expandedWordsArray = new string[words.Length == 0 ? 1 : words.Length * 2];
+            int[] expandedCountArray = new int[words.Length == 0 ? 1 : words.Length * 2];
+
+            i = 0;
+            copyOldArray:
+            {
+                if (i == words.Length)
+                {
+                    words = expandedWordsArray;
+                    repeatedWordsCount = expandedCountArray;
+                    goto addWordToArray;
+                }
+
+                expandedWordsArray[i] = words[i];
+                expandedCountArray[i] = repeatedWordsCount[i];
+
+                i++;
+                goto copyOldArray;
+            }
+        }
+
+
+        addWordToArray:
+        {
+            words[wordsCount] = word;
+            repeatedWordsCount[wordsCount] = 1;
+            word = String.Empty;
+            wordsCount++;
+        }
+    }
+
+    goto readFile;
+}
+
+
+finishReading:
+{
+    streamReader.Close();
+}
+
+
+int j = 1;
+sortArray:
+{
+    int currentEl = repeatedWordsCount[j];
+    word = words[j];
+
+    int k = j - 1;
+    checkPreviousElements:
+    {
+        if (k >= 0 && repeatedWordsCount[k] < currentEl)
+        {
+            repeatedWordsCount[k + 1] = repeatedWordsCount[k];
+            words[k + 1] = words[k];
+            k--;
+            goto checkPreviousElements;
+        }
+    }
+
+    repeatedWordsCount[k + 1] = currentEl;
+    words[k + 1] = word;
+    j++;
+
+    if (j < wordsCount)
+    {
+        goto sortArray;
+    }
+}
+
+
+StreamWriter streamWriter = new StreamWriter(outputPath);
+j = 0;
+
+writeWordToFile:
+{
+    streamWriter.WriteLine(words[j] + " - " + repeatedWordsCount[j]);
+    j++;
+
+    if (j < maxWordsCount && j < wordsCount)
+    {
+        goto writeWordToFile;
+    }
+}
+
+streamWriter.Close();
---
+++ Vakor.Paradigms.Lab1/task2.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.IO;
+
+string inputPath = "../../../input.txt";
+string outputPath = "../../../output2.txt";
+
+const int linesPerPage = 45;
+
+string[] words = Array.Empty<string>();
+int[][] wordPages = Array.Empty<int[]>();
+int[] repeatedWordsCount = Array.Empty<int>();
+
+int currentPage = 0;
+int strCount = 0;
+int wordsCount = 0;
+
+int i = 0;
+
+StreamReader streamReader = new StreamReader(inputPath);
+readFile:
+{
+    if (streamReader.EndOfStream)
+    {
+        goto endReading;
+    }
+
+    String str = streamReader.ReadLine();
+
+    if (strCount % linesPerPage == 0)
+    {
+        currentPage++;
+    }
+
+    strCount++;
+
+    int j = 0;
+
+    string word = String.Empty;
+    checkString:
+    {
+        if (j == str.Length)
+        {
+            goto endCheckingString;
+        }
+
+        char symbol = str[j];
+
+        if (symbol is >= 'A' and <= 'Z')
+        {
+            word += (char) (symbol + 32);
+
+            if (j + 1 < str.Length)
+            {
+                goto endCheckingString;
+            }
+        }
+        else if (symbol is >= 'a' and <= 'z' || word != "" && symbol is '-' or '\'')
+        {
+            word += symbol;
+
+            if (j + 1 < str.Length)
+            {
+                goto endCheckingString;
+            }
+        }
+
+        if (word != "")
+        {
+            i = 0;
+            checkWordInArray:
+            {
+                if (i == wordsCount)
+                {
+                    goto addNewWord;
+                }
+
+                if (word == words[i])
+                {
+                    word = "";
+                    if (repeatedWordsCount[i] > 100)
+                    {
+                        goto endCheckingString;
+                    }
+
+                    repeatedWordsCount[i]++;
+                    if (repeatedWordsCount[i] <= wordPages[i].Length)
+                    {
+                        wordPages[i][repeatedWordsCount[i] - 1] = currentPage;
+                    }
+                    else
+                    {
+                        int[] pagesTmp = new int[repeatedWordsCount[i] * 2];
+                        int p = 0;
+                        copyPages:
+                        {
+                            pagesTmp[p] = wordPages[i][p];
+                            p++;
+                            if (p < repeatedWordsCount[i] - 1)
+                            {
+                                goto copyPages;
+                            }
+                        }
+                        wordPages[i] = pagesTmp;
+                        wordPages[i][repeatedWordsCount[i] - 1] = currentPage;
+                    }
+
+                    goto endCheckingString;
+                }
+
+                i++;
+                goto checkWordInArray;
+            }
+
+            addNewWord:
+            {
+                if (wordsCount != words.Length)
+                {
+                    goto addWordToArray;
+                }
+            }
+
+            // expanding array
+            {
+                string[] expandedWordsArray = new string[words.Length == 0 ? 1 : words.Length * 2];
+                int[][] expandedPagesArray = new int[words.Length == 0 ? 1 : words.Length * 2][];
+                int[] expandedRepeatedArray = new int[words.Length == 0 ? 1 : words.Length * 2];
+
+                i = 0;
+                copyOldArray:
+                {
+                    if (i == words.Length)
+                    {
+                        words = expandedWordsArray;
+                        repeatedWordsCount = expandedRepeatedArray;
+                        wordPages = expandedPagesArray;
+
+                        goto addWordToArray;
+                    }
+
+                    expandedWordsArray[i] = words[i];
+                    expandedPagesArray[i] = wordPages[i];
+                    expandedRepeatedArray[i] = repeatedWordsCount[i];
+
+                    i++;
+                    goto copyOldArray;
+                }
+            }
+
+
+            addWordToArray:
+            {
+                words[wordsCount] = word;
+                repeatedWordsCount[wordsCount] = 1;
+                wordPages[wordsCount] = new[] {currentPage};
+                word = String.Empty;
+                wordsCount++;
+            }
+        }
+    }
+    endCheckingString:
+    {
+        j++;
+        if (j < str.Length)
+        {
+            goto checkString;
+        }
+    }
+
+
+    goto readFile;
+}
+
+endReading:
+{
+    streamReader.Close();
+}
+
+i = 1;
+sort:
+{
+    int currentElement = repeatedWordsCount[i];
+    string word = words[i];
+    int[] currPages = wordPages[i];
+    var k = i - 1;
+    checkPreviousElements:
+    {
+        if (k >= 0)
+        {
+            int symbol = 0;
+            compareWords:
+            {
+                if (symbol == words[k].Length || words[k][symbol] < word[symbol])
+                {
+                    goto addLastElement;
+                }
+
+                if (symbol + 1 < word.Length && words[k][symbol] == word[symbol])
+                {
+                    symbol++;
+                    goto compareWords;
+                }
+            }
+
+            repeatedWordsCount[k + 1] = repeatedWordsCount[k];
+            words[k + 1] = words[k];
+            wordPages[k + 1] = wordPages[k];
+            k--;
+            goto checkPreviousElements;
+        }
+    }
+    addLastElement:
+    {
+        repeatedWordsCount[k + 1] = currentElement;
+        words[k + 1] = word;
+        wordPages[k + 1] = currPages;
+        i++;
+    }
+
+    if (i < wordsCount)
+    {
+        goto sort;
+    }
+}
+
+StreamWriter streamWriter = new StreamWriter(outputPath);
+i = 0;
+writeToFile:
+{
+    if (repeatedWordsCount[i] <= 100)
+    {
+        streamWriter.Write(words[i] + " - " + wordPages[i][0]);
+        int j = 1;
+        writePages:
+        {
+            if (j == repeatedWordsCount[i])
+            {
+                goto endWritingPages;
+            }
+
+            if (wordPages[i][j] != wordPages[i][j - 1])
+            {
+                streamWriter.Write(", " + wordPages[i][j]);
+            }
+
+            j++;
+            goto writePages;
+        }
+
+        endWritingPages:
+        {
+            streamWriter.WriteLine();
+        }
+    }
+
+    i++;
+    if (i < wordsCount)
+    {
+        goto writeToFile;
+    }
+}
+
+streamWriter.Close();
